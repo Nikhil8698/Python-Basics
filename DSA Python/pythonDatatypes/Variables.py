c75c4778907793551dda2486d84f5ae4bf61a11d
Nikhil8698 <121568315+Nikhil8698@users.noreply.github.com>
2024-08-06 23:02:21 +0530

    Add files via upload

---
+++ DSA Python/pythonDatatypes/Variables.py
@@ -0,0 +1,13 @@
+"""#Variables cannot start with number
+1a=10
+#Variables cannot have spaces
+first variable = 10
+#Variables cannot use keywords
+int=False
+#Variables can start with underscore
+_a=10
+#Variables can also be taken from  commnd line using input function"""
+a=input("Type something")
+#know the type of the variable by type function
+a=10
+print(type(a))
